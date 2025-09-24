6ab15b4963a46d0200798a0dca5c08c9b9fcdcc5
Leon <levzagretsky@gmail.com>
2025-09-25 01:00:53 +0300

    Homework9 solution v3

--- homeworks/hw9/candles/__init__.py
+++
@@ -1,8 +0,0 @@
-def remove_previous_symbol(text):
-    while '#' in text:
-        index = text.find('#')
-        if index > 0:
-            text = text[:index - 1] + text[index + 1:]
-        else:
-            text = text[1:]
-    return text
