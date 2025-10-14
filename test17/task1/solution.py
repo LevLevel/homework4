69ab6f6e65e443455c1e06bc8d67d4fe67260e25
Leon <levzagretsky@gmail.com>
2025-10-14 19:25:19 +0300

    Task 1

---
+++ test17/task1/solution.py
@@ -0,0 +1,16 @@
+a="Skoda, simply clever"
+
+first_char = a[0]
+print(f"First char: '{first_char}'")
+last_char = a[-1]
+print(f"Last char: '{last_char}'")
+third_char = a[2]
+print(f"Third char: '{third_char}'")
+third_end_char=a[-3]
+print(f"Third end char: '{third_end_char}'")
+a_len = len(a)
+print(f"Length of string: '{a_len}'")
+reversed_a = a[::-1]
+print(f"Reversed string: '{reversed_a}'")
+eight_chars = a[:8]
+print(f"Eight chars: '{eight_chars}'")
