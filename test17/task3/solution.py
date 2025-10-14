eccac569c0e73b9ee6a5229df47acc49282b60e5
Leon <levzagretsky@gmail.com>
2025-10-14 19:45:35 +0300

    Task 3

---
+++ test17/task3/solution.py
@@ -0,0 +1,12 @@
+def sum_numbers(n):
+    result = 0
+    for n in range(1, n + 1):
+        result += n
+    print(f"Sum to {n} = {result}")
+    return result
+
+assert sum_numbers(1) == 1
+assert sum_numbers(2) == 3
+assert sum_numbers(8) == 36
+assert sum_numbers(22) == 253
+assert sum_numbers(100) == 5050
